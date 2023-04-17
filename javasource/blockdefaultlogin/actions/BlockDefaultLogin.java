3f80cb7abd2ae4b62d44d4b48aa417bcedaf13f0
Arjen Lammers <arjen.lammers@mendix.com>
2023-04-17 15:38:06 +0200

    Converted to 9.18 for Mendix 10 compat.

--- javasource/blockdefaultlogin/actions/BlockDefaultLogin.java
+++ javasource/blockdefaultlogin/actions/BlockDefaultLogin.java
@@ -43,6 +43,7 @@ public class BlockDefaultLogin extends CustomJavaAction<java.lang.Boolean>
 
 	/**
 	 * Returns a string representation of this action
+	 * @return a string representation of this action
 	 */
 	@java.lang.Override
 	public java.lang.String toString()
